4a6ab5f44267e8ba5386daca1735d208924aa055
David Acosta <davidacosta25081@yahoo.com>
2019-02-19 04:47:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,4 @@ class Song
 
   attr_accessor :title, :artist
 
-  def initialize (title)
-    @title = title
-
-
-  end
-
 end
-
-drunken_love = Song.new ("Drunken Love")
-beyonce = Artist.new ("Beyonce")
-drunken_love.artist = beyonce
